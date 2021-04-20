77a63612010c23591b351e8a22a4a33e05511a23
omeryaldirgan <omeryaldirgan@hotmail.com>
2021-04-20 22:06:02 +0300

    header ve footer ayarlandı

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
-module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+
+function px(pixels) {
+  return `${pixels / 16}rem`;
 }
+
+module.exports = {
+  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontSize: {
+      xs: px(12),
+      sm: px(14),
+      base: px(15),
+      lg: px(18),
+      xl: px(20),
+      "2xl": px(24),
+      "3xl": px(30),
+      "4xl": px(36),
+      "5xl": px(48),
+      "6xl": px(64),
+    },
+    extend: {
+      screens: {
+        xs: "480px",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
+
+
